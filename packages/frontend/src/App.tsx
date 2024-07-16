a12acd84125bf8849d95f8385929086e0517e37e
vzhang03 <vzhang@vassar.edu>
2024-07-16 10:58:14 -0400

    Moving files into frontend folder

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+// import JsPsychMetadata from '../../metadata/src/index'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
 
   return (
     <>
