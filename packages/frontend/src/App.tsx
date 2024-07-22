3317240851ec71d65ff7b28c51446b8f6671e616
vzhang03 <vzhang@vassar.edu>
2024-07-22 10:04:31 -0400

    Pushing broken import

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import Metadata from "./pages/Metadata";
 import Options from './pages/Options.tsx'
-
+import { JsPsychMetadata } from 'metadata';
 
 import './App.css'
 
-//var jsPsychMetadata = new JsPsychMetadata();
+// var jsPsychMetadata = new JsPsychMetadata();
 
 function App() {
   return (
     <>
-    <Metadata />
-    <Options />
+      <Metadata />
+      <Options />
     </>
   )
 }
