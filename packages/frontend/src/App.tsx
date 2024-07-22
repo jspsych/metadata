c6bd248da4f8a31400b9b0857f2a51b28f7f53d5
vzhang03 <vzhang@vassar.edu>
2024-07-22 14:58:37 -0400

    Added typescript outputs to the build, didn't configure build commands yet but having issues

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import Metadata from "./pages/Metadata";
 import Options from './pages/Options.tsx'
 import { JsPsychMetadata } from 'metadata';
-
 import './App.css'
 
-// var jsPsychMetadata = new JsPsychMetadata();
+var jsPsychMetadata = new JsPsychMetadata();
 
 function App() {
+  console.log(jsPsychMetadata.getMetadata());
   return (
     <>
       <Metadata />
